a32362c61da14647c3a3804865d1a8d38bd0e3b3
Kadircan Karaca <49828425+kdrkrc19@users.noreply.github.com>
2022-12-22 12:58:59 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -11,12 +11,13 @@ public:
 	ofstream personelYaz;
 	ifstream mesaiOku;
 	ofstream mesaiYaz;
+	ofstream mesaiYaz2;
 	
 	void personelEkle(int);
 	void maasOde();
 	void mesaiEkle();
 	int mesaiUcretleri[50];
-	string personeller[50];
+	string personelMesaiDuzenleme[50];
 	int maasGiderleri[50];
 
 };
@@ -28,6 +29,7 @@ void Personel::personelEkle(int i) {
 
 	personelYaz.open("personeller.txt", ios::app);
 	personelOku.open("personeller.txt", ios::in);
+	mesaiYaz.open("personelMesai.txt", ios::app);
 
 	cout << "Eklenecek Personel Adi : ";
 	cin >> pAd;
@@ -37,35 +39,46 @@ void Personel::personelEkle(int i) {
 	cin >> pYas;
 	cout << "Eklenecek Personel No : ";
 	cin >> pNo;
+
 	personelYaz << "[" << pNo << "] --> " << pAd << " " + pSoyad << " -- " << pYas << endl;
-	mesaiUcretleri[i] = 0;
+	mesaiYaz << "[" << pNo << "] --> " << pAd << " " + pSoyad << " -- " << pYas << " -- 0" << endl;
+
 	personelYaz.close();
 	personelOku.close();
+	mesaiYaz.close();
 }
 
 
 void Personel::mesaiEkle() {
 	string pAd, pSoyad, veri;
 	int sayac = 0;
-	int mesaiSaati;
-	int index;
-	int ucret;
+	int index, ucret, mesaiSaati, toplamMesai, mesai1, mesaiSecim, satirSayisi = 0;
+	string toplamMesaiStr;
 
+	mesaiYaz2.open("personelMesai2.txt", ios::app);
+	mesaiYaz.open("personelMesai.txt", ios::app);
+	mesaiOku.open("personelMesai.txt", ios::in);
 	personelYaz.open("personeller.txt", ios::app);
 	personelOku.open("personeller.txt", ios::in);
 
-	if (personelOku.is_open()) {
-		while (!personelOku.eof()) {
-			if (sayac % 6 == 0 & sayac != 0) {	// Okurken Alt satýra geçmesi için
+	if (mesaiOku.is_open()) {
+		while (!mesaiOku.eof()) {				// Dosya sonuna ulaþýp ulaþamadýðýný gösteren
+			if (sayac % 8 == 0 && sayac != 0) {	// Okurken Alt satýra geçmesi için
 				cout << endl;
+				satirSayisi++;
 			}
-			else if ((sayac % 1 == 0 || sayac % 2 == 0 || sayac % 3 == 0 || sayac % 4 == 0 || sayac % 5 == 0) & (sayac != 0)) {	// okurken elemanlar arasýnda boþluk býrakmasý için
+			else if ((sayac % 1 == 0 || sayac % 2 == 0 || sayac % 3 == 0 || sayac % 4 == 0 || sayac % 5 == 0 
+				|| sayac % 6 == 0 || sayac % 7 == 0) && (sayac != 0)) {									// okurken elemanlar arasýnda boþluk býrakmasý için
 				cout << "  ";
 			}
-			personelOku >> veri;
+			mesaiOku >> veri;
+			personelMesaiDuzenleme[sayac] = veri;
 			cout << veri;
 			sayac++;
 		}
+	}
+	for (int i = 0; i < 10; i++) {
+		cout << personelMesaiDuzenleme[i] << endl; //7-15-23-31-39 mesai ücreti yani 7+8xIndis
 	}
 	cout << "Mesai eklemek istedigin kisinin numarasi : ";
 	cin >> index;
@@ -74,14 +87,50 @@ void Personel::mesaiEkle() {
 	cin >> mesaiSaati;
 	cout << "Mesai saatlik ucreti : ";
 	cin >> ucret;
-	mesaiUcretleri[index] = (mesaiUcretleri[index] + (mesaiSaati* ucret));
+	
+	if (index == 0)
+		mesaiSecim = 7;
+	else
+		mesaiSecim = 7 + (index * 8);
 
-	for (int i = 0; mesaiUcretleri[i] != -858993460; i++)
-	{
-		cout << (personeller[i]) << " - " << mesaiUcretleri[i] << " tl" << endl;
+	string strMesai1 = personelMesaiDuzenleme[mesaiSecim];
+	mesai1 = stoi(strMesai1);								// String to int
+	
+	toplamMesai = (mesai1 + (mesaiSaati * ucret));
+	
+	toplamMesaiStr = to_string(toplamMesai);
+	//cout << "Toplam Mesai : " << toplamMesai << endl;
+	personelMesaiDuzenleme[mesaiSecim] = toplamMesaiStr;
+
+	/*cout << "strMesai1 : " << strMesai1 << endl;
+	cout << "personelMesaiDuzenleme[mesaiSecim] : " << personelMesaiDuzenleme[mesaiSecim] << endl;
+	cout << "Mesai1 : " << mesai1 << endl;*/
+	for (int i = 0; i < 30; i++) {
+		cout << personelMesaiDuzenleme[i] << endl; //7-15-23-31-39 mesai ücreti yani 7+8xIndis
 	}
+	int satirIndis = 0;
+	while (satirIndis < satirSayisi) {
+		int diziIndis = 0;
+
+		if (satirIndis == 0)
+			diziIndis = 0;
+		else if (satirIndis == 1)
+			diziIndis = 8;
+		else 
+			diziIndis = 7 + (satirIndis * 8);
+		mesaiYaz2 << personelMesaiDuzenleme[diziIndis] << " " << personelMesaiDuzenleme[diziIndis + 1] << " " << personelMesaiDuzenleme[diziIndis + 2] << " "
+				 << personelMesaiDuzenleme[diziIndis + 3] << " " << personelMesaiDuzenleme[diziIndis + 4] << " " << personelMesaiDuzenleme[diziIndis + 5] << " "
+			     << personelMesaiDuzenleme[diziIndis + 6] << " " << personelMesaiDuzenleme[diziIndis + 7] << endl;
+		satirIndis++;
+	}
+
+	
+	mesaiYaz2.close();
+	mesaiOku.close();
+	mesaiYaz.close();
 	personelYaz.close();
 	personelOku.close();
+	remove("personelMesai.txt");
 }
 
 
@@ -94,10 +143,10 @@ void Personel::maasOde() {
 	double odenenTutar = 0;
 	if (personelOku.is_open()) {
 		while (!personelOku.eof()) {
-			if (sayac % 6 == 0 & sayac != 0) {	// Okurken Alt satýra geçmesi için
+			if (sayac % 6 == 0 && sayac != 0) {	// Okurken Alt satýra geçmesi için
 				cout << endl;
 			}
-			else if ((sayac % 1 == 0 || sayac % 2 == 0 || sayac % 3 == 0 || sayac % 4 == 0 || sayac % 5 == 0) & (sayac != 0)) {	// okurken elemanlar arasýnda boþluk býrakmasý için
+			else if ((sayac % 1 == 0 || sayac % 2 == 0 || sayac % 3 == 0 || sayac % 4 == 0 || sayac % 5 == 0) && (sayac != 0)) {	// okurken elemanlar arasýnda boþluk býrakmasý için
 				cout << "  ";
 			}
 			personelOku >> veri;
@@ -111,16 +160,20 @@ void Personel::maasOde() {
 	cout << mesaiUcretleri[pNo - 1] << endl;
 	cout << "Ne Kadar Odensin ? ";
 	cin >> odenenTutar;
-//	maasGiderleri[pNo] = odenenTutar;
+
 	personelYaz.close();
 	personelOku.close();
 }
+
 
 class Muhasebe {
 public:
 	int personelMaas;
 	int personelEkMesai;
 };
+
+
+
 
 
 int main() {
